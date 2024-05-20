70e7105e8e364baf7eaba21b45df98f484a11c9f
gifortunato2024 <168582782+gifortunato2024@users.noreply.github.com>
2024-05-20 16:05:25 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,8 @@
 import streamlit as st 
 import pandas as pd 
-import seaborn as sns 
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("arquivo.csv")
 
-grafico = sns.barplot(data=df, x="Country", y="Financial allocations($ billion)")
+grafico = plt.plot(df["Country"], df["Financial allocations($ billion)"])
 st.pyplot(grafico)
